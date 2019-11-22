16eac38a20c11d29db1531d7d457f3383fd2cd86
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-11-22 05:28:15 -0500

    added some incomplete routing and a default friend

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+// 4. Your `apiRoutes.js` file should contain two routes:
+
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var arrayOfObjects = require("../data/arrayOfObjects");
+//========================================================================
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(tableData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        
+    
+    });
+
+    // app.post("/api/clear", function(req, res) {
+    //   tableData.length = 0;
+    //   waitListData.length = 0;
+    //   res.json({ ok: true });
+    // });
+  };
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
+// 2. Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
 var express = require("express");
-
-// EXPRESS CONFIGURATION
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// ROUTER The below points our server to a series of "route" files.
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
